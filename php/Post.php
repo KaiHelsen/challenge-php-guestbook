3237720578b61837a4a5e396bd1bd18d2e585d5e
KaiHelsen <kai@Helsen.info>
2021-02-26 11:14:56 +0100

    little bit of cleanup and implemented basic setup for findsmileys function

--- php/Post.php
+++ php/Post.php
@@ -14,6 +14,8 @@ class Post
         $this->title = $this->parseString($title, $badWords);
         $this->date = $date;
         $this->content = $this->parseString($content, $badWords);
+        $this->content = $this->findSmileys($this->content);
+
         $this->authorName = $this->parseString($authorName, $badWords);
     }
 
@@ -39,6 +41,10 @@ class Post
 
     private function parseString(string $input, array $badWords) : string
     {
+        $myString = $input;
+        $myString = $this->validateString($myString);
+        $myString = $this->cleanseString($myString, $badWords);
+
         return $this->cleanseString($this->validateString($input), $badWords);
     }
 
@@ -49,7 +55,28 @@ class Post
 
     private function cleanseString(string $input, array $badWords):string
     {
+        //just looking for naughty words in and of themselves will lead to the scunthorpe problem
+        //the best and easiest solution I can think of is to at LEAST filter for the bad word, but only replace if there is NOT a space in front of it
+        //sure, that could still lead to problems but at least Scunthorpe is safe.
+        //penistone is still in trouble but I'd have to write exceptions.
+
+//        array_walk($badWords, static function(&$word){$word = " " . $word;});
         return str_replace($badWords, '****', $input);
+
+        //okay so that's not great
+
+    }
+
+    private function findSmileys(string $input){
+        $smileys = [
+            ':)' => '🙂',
+            ':-)' => '🙂',
+            ';-)' => '😉',
+            ';)' => '😉',
+            ':-o' => '😮',
+            ':o' => '😮',
+        ];
+        return str_ireplace(array_keys($smileys), $smileys, $input);
     }
 
 
